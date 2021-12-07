181c382c8629bc9ef1cae85588e375d46714453d
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-07 10:49:10 -0600

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -2,26 +2,55 @@ const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+  //  res.json([{},{}]) good way to test to see if endpoint is returning data. also try to throw error: throw new Error('something went wrong!')
+    res.json('get accounts')
+  } catch (err) {
+    next({ status: 422, message: 'this is horrible'})
+  }
 })
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('get accounts by id')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('post accounts')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('update accounts')
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    res.json('delete accounts')
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
