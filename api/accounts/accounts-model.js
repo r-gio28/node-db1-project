3d112795b41e8dac7ff767f562b8fc15bcd90857
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-07 18:58:06 -0600

    MVP

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -11,19 +11,20 @@ const getById = id => {
 }
 
 const create = async account => {
-  //equivalent to insert into accounts ( name, budget ) values ('foo', 1000);
+  //equivalent to: insert into accounts ( name, budget ) values ('foo', 1000);
     const [id] = await db('accounts').insert(account)
     return getById(id)
 }
 
-const updateById = (id, account) => {
+const updateById = async (id, account) => {
   // DO YOUR MAGIC
-
+    await db('accounts').where('id', id).update(account)
+    return getById(id)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
-
+  // equivalent to: delete from accounts where id = 1
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -22,26 +22,29 @@ router.get('/:id', md.checkAccountId, async (req, res, next) => {
 router.post('/', md.checkAccountPayload, md.checkAccountNameUnique, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    const newAccount = await Account.create(req.body)
+    const newAccount = await Account.create({ name: req.body.name.trim(), budget: req.body.budget, })
     res.status(201).json(newAccount)
   } catch (err) {
     next(err)
   }
 })
 
-router.put('/:id', md.checkAccountId, md.checkAccountPayload, md.checkAccountNameUnique, (req, res, next) => {
+router.put('/:id', md.checkAccountId, md.checkAccountPayload, async (req, res, next) => {
   // DO YOUR MAGIC
+  const updated = await Account.updateById(req.params.id, req.body)
+  res.json(updated)
   try {
-    res.json('update accounts')
+    res.json('update account')
   } catch (err) {
     next(err)
   }
 });
 
-router.delete('/:id', md.checkAccountId, (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   try {
-    res.json('delete account')
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
